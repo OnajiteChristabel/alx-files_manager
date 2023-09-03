f54479e870aa3850a69411ff6be56f535cbddd79
OnajiteChristabel <111015651+OnajiteChristabel@users.noreply.github.com>
2023-09-03 10:09:11 +0100

    Update redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -25,3 +25,9 @@ class RedisClient {
     const value = await redisGet(key);
     return value;
   }
+    // set key value pair to redis server
+  async set(key, value, time) {
+    const redisSet = promisify(this.client.set).bind(this.client);
+    await redisSet(key, value);
+    await this.client.expire(key, time);
+    }
